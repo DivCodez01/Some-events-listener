bc0ad256b4d402d3ca256e923fa3c0c4ae092cb7
divin-gakiza <divin.gakiza01@gmail.com>
2024-05-15 01:08:40 +0200

    decrement image

--- imageChange.js
+++ imageChange.js
@@ -2,21 +2,33 @@ const img = document.querySelector("img");
 const prvButton = document.querySelector("button:first-child");
 const nxtButton = document.querySelector("button:last-child");
 const header = document.querySelector("h1");
-let image = imgData[0].image;
-let convert = imgData[0].convert;
-let title = imgData[0].title;
 let nbr = 0;
 
-// const prevElms = (data, img, conv) => {
-// }
-// prevElms(image, convert);
+const prevElms = () => {
+    nbr--;
+    if (nbr < 0) {
+        nbr = imgData.length - 1;
+        firstImage(nbr);
+    } else {
+        firstImage(nbr);
+    }
+}
 
-const imgs = (img, conv, headDes) => {
-    let src = `${img}${conv}`;
-    console.log(src)
-    header.innerHTML = `${headDes}`;
+const firstImage = (countImage) => {
+    let image = imgData[countImage].image;
+    let convert = imgData[countImage].convert;
+    let title = imgData[countImage].title;
+    let source = imgData[countImage].source;
+    let alt = imgData[countImage].altenate;
+    header.innerHTML = `${title}`;
+    let src = `${source}${image}${convert}`;
+    img.setAttribute("alt", `${alt}`);
     img.src = src;
     console.log(img)
 }
 
-imgs(image, convert, title);
+firstImage(nbr);
+
+prvButton.addEventListener("click", () => {
+    prevElms();
+})
--- imageChangeData.js
+++ imageChangeData.js
@@ -1,30 +1,30 @@
 const imgData = [
     {
-        image: "IMG2",
-        altenate: "Image",
-        title: "IMAGE",
+        image: "IMG1",
+        altenate: "Image 1",
+        title: "IMAGE 1",
         convert: ".JPG",
-        source: "images"
+        source: "images/"
     },
     {
         image: "IMG2",
-        altenate: "Image",
-        title: "IMAGE",
+        altenate: "Image 2",
+        title: "IMAGE 2",
         convert: ".JPG",
-        source: "images"
+        source: "images/"
     },
     {
         image: "IMG3",
-        altenate: "Image",
-        title: "IMAGE",
+        altenate: "Image 3",
+        title: "IMAGE 3",
         convert: ".JPG",
-        source: "images"
+        source: "images/"
     },
     {
         image: "IMG4",
-        altenate: "Image",
-        title: "IMAGE",
+        altenate: "Image 4",
+        title: "IMAGE 4",
         convert: ".JPG",
-        source: "images"
+        source: "images/"
     }
 ];
