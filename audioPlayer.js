93ab83f321972b1d750f56c9369406a8bfc4b388
divin-gakiza <divin.gakiza01@gmail.com>
2024-05-15 21:47:41 +0200

    insert elms

--- audioPlayer.js
+++ audioPlayer.js
@@ -1,2 +1,7 @@
 const songTitle = document.querySelector("h1");
 const artistTitle = document.querySelector("p");
+const disc = document.querySelector(".disc");
+const musicRange = document.getElementById("range");
+const currentMusic = document.querySelector("label:first-child");
+const cmusicDuratin = document.querySelector("label:first-child");
+console.log(currentMusic);
