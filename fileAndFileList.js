4d8c52310498aa8d527b5ef844b1122f38add2a7
divin-gakiza <divin.gakiza01@gmail.com>
2024-05-25 16:46:25 +0200

    input func

--- fileAndFileList.js
+++ fileAndFileList.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("inputFile").addEventListener("change", filesPicked);
 
-    document.getElementById("btnToggle").addEventListener("click", toggleInput);
-    document.getElementById("btnPick").addEventListener("click", askForFiles);
-    document.getElementById("btnInfo").addEventListener("click", showFileIngfo);
+    // document.getElementById("btnToggle").addEventListener("click", toggleInput);
+    // document.getElementById("btnPick").addEventListener("click", askForFiles);
+    // document.getElementById("btnInfo").addEventListener("click", showFileIngfo);
 });
+
+function filesPicked(ev) {
+    let input = ev.target;
+    let files = input.files;
+    console.log(files);
+}
+
