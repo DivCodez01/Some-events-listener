d0cbb3cb6e1e2a6ec136c9b3e2bdf8d63b0434ad
divin-gakiza <divin.gakiza01@gmail.com>
2024-05-25 18:31:58 +0200

    add other div

--- firstToOtherTwoVideosOfEvents.js
+++ firstToOtherTwoVideosOfEvents.js
@@ -4,3 +4,12 @@ divs.forEach(div => {
         console.log("hi")
     })
 })
+
+const newDiv = document.createElement("div");
+newDiv.style.width = "200px";
+newDiv.style.height = "200px"
+newDiv.style.backgroundColor = "purple"
+newDiv.addEventListener("click", () => {
+    console.log("hi")
+})
+document.body.appendChild(newDiv);
